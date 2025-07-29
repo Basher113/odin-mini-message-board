e043c194affc275c44047433d86ea6b686169213
Basher113 <147272612+Basher113@users.noreply.github.com>
2025-07-30 01:37:28 +0800

    Add /new POST routes with form submission
    - Handle form submission on POST /new and save message
    - Redirect to homepage after submission

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 const indexRouter = require("./routes/index");
 const newRouter = require("./routes/new");
 
+
+app.use(express.urlencoded({ extended: true }));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
--- routes/new.js
+++ routes/new.js
@@ -1,8 +1,15 @@
 const {Router} = require("express");
+const messages = require("../messages");
 const newRouter = Router();
 
 newRouter.get("/", (req, res) => {
   res.render("form")
 });
+
+newRouter.post("/", (req, res) => {
+  const {text, user} = req.body;
+  messages.push({text, user, added: new Date()});
+  res.redirect("/");
+})
 
 module.exports = newRouter;
