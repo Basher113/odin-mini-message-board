bc376c03954a83bc36e4bf682fef810a46b200c4
Basher113 <147272612+Basher113@users.noreply.github.com>
2025-07-30 01:09:44 +0800

    Add start express app, add index router and views
    - Use ejs to render template views
    - Add form template and include it in index view

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("node:path");
+
+const app = express();
+
+const indexRouter = require("./routes/index");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+
+
+
+app.use("/", indexRouter);
+
+app.listen(8000);
