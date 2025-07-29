d7e78c335b14ae3bb553d9d131960efc4f8811b0
Basher113 <147272612+Basher113@users.noreply.github.com>
2025-07-30 01:19:05 +0800

    Add /new route and render template form

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const path = require("node:path");
 const app = express();
 
 const indexRouter = require("./routes/index");
+const newRouter = require("./routes/new");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 
-
-
+app.use("/new", newRouter);
 app.use("/", indexRouter);
 
 app.listen(8000);
